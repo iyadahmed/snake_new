0951aca5d77048a72cb77443ba080f9b031944da
Iyad Ahmed <iyadahmed430@gmail.com>
2022-11-26 18:55:54 +0200

    Update comments, set initial snake length to 1, and grow snake after eating food

--- snake.py
+++ snake.py
@@ -4,20 +4,23 @@ import random
 
 pygame.init()
 
-BLOCK_SIZE = 20
-NUM_X_BLOCKS = 20
-NUM_Y_BLOCKS = 20
+BLOCK_SIZE = 20 # Size of snake or food (also size of game grid square)
+NUM_X_BLOCKS = 20 # Window width is a whole (integer) multiple of block size
+NUM_Y_BLOCKS = 20 # same for window height
 WINDOW_WIDTH = NUM_X_BLOCKS * BLOCK_SIZE
 WINDOW_HEIGHT = NUM_Y_BLOCKS * BLOCK_SIZE
 
+# Create window
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+# Set snake location to center of screen (we divide number of blocks by two)
 snake_head_x = (NUM_X_BLOCKS // 2) * BLOCK_SIZE
 snake_head_y = (NUM_Y_BLOCKS // 2) * BLOCK_SIZE
 snake_direction = "up"
-snake_body = [] # List of snake block locations
-snake_length = 10
+snake_body = [] # List of snake's body blocks' locations
+snake_length = 1
 
+# Create food in a random location inside game window
 food_x = random.randint(0, NUM_X_BLOCKS - 1) * BLOCK_SIZE
 food_y = random.randint(0, NUM_Y_BLOCKS - 1) * BLOCK_SIZE
 
@@ -44,7 +47,7 @@ while is_game_running:
                 is_game_running = False
 
 
-    # Move snake
+    # Move snake based on direction
     if snake_direction == "up":
         snake_head_y -= BLOCK_SIZE
     elif snake_direction == "right":
@@ -54,13 +57,19 @@ while is_game_running:
     elif snake_direction == "down":
         snake_head_y += BLOCK_SIZE
     
+    # If snake head location is the same as food, then create new food
+    # in a different location
     if snake_head_x == food_x and snake_head_y == food_y:
         food_x = random.randint(0, NUM_X_BLOCKS - 1) * BLOCK_SIZE
         food_y = random.randint(0, NUM_Y_BLOCKS - 1) * BLOCK_SIZE
+        snake_length += 1
     
-    # Add new snake block
+    # Store snake's head location in snake body list; so it can be drawn later;
+    # drawing the last N snake head locations creates the illusion
+    # of snake's movement
     snake_body.append((snake_head_x, snake_head_y))
-    # If greater than snake length, delete oldest snake block
+    # If length of snake body is greater than snake length, delete oldest snake block
+    # also part of the illusion
     if len(snake_body) > snake_length:
         snake_body.pop(0)
 
